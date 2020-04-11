aace09af61a23ec20787d94088b706e00f7e6ef0
Matt Berg <matthew.berg@nike.com>
2020-04-11 12:47:27 -0700

    Add material and base layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
 import "./App.css";
 import Board from "./components/Board/Board";
+import {
+  AppBar,
+  CssBaseline,
+  Container,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <Board />
-    </div>
+    <>
+      <CssBaseline />
+      <div className="App">
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="h1">
+              War Game
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth="lg">
+          <Board />
+        </Container>
+      </div>
+    </>
   );
 }
 
